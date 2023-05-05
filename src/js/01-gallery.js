4fc4fb6073e9e1dcc8991fad9d20c8590421f38b
Oleksandr <live.gp@gmail.com>
2023-05-05 19:34:13 +0300

    1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}" 
+                        alt="${description}" />
+                    </a>
+                </li>
+                `;
+    })
+    .join('');
+}
+
+const box = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    enableKeyboard: true,
+    showCounter: false,
+    scrollZoom: false,
+    close: false,
+});
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    box.open(event.target.parentNode.href);
+}
 console.log(galleryItems);
