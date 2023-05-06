5cd93d5291a31f757bd1eb677fbc8df09e1eaca0
Oleksandr <live.gp@gmail.com>
2023-05-07 00:05:56 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,23 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('play', () => {
+  console.log('play');
 });
 
-player.on('play', function () {
-  console.log('played the video!');
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
 });
+
+player.on('timeupdate', throttle(function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}, 1000));  
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
