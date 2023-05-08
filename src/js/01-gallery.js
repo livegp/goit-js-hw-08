151ba52296bc008202e8687aa776ede116df327f
Oleksandr <live.gp@gmail.com>
2023-05-08 19:05:13 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,30 +5,32 @@ import { galleryItems } from './gallery-items';
 const gallery = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+if (gallery) {
+    gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-function createGalleryMarkup(galleryItems) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-            return `
-                <li class="gallery__item">
-                    <a class="gallery__link" href="${original}">
-                        <img
-                        class="gallery__image"
-                        src="${preview}" 
-                        alt="${description}" />
-                    </a>
-                </li>
-                `;
-    })
-    .join('');
+    const box = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        enableKeyboard: true,
+        showCounter: false,
+        scrollZoom: false,
+        close: false,
+    });
 }
 
-const box = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    enableKeyboard: true,
-    showCounter: false,
-    scrollZoom: false,
-    close: false,
-});
+function createGalleryMarkup(items) {
+    return items
+        .map(
+            ({ preview, original, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}" />
+            </a>
+        </li>
+        `
+        )
+        .join('');
+}
